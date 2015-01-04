4c9e679e436b21348dd82787683ea38622ed9c40
Lockout87 <lockout87@gmail.com>
2015-01-03 23:36:49 -0800

    Created several more classes to fill out information

--- GameStuff/main.py
+++ GameStuff/main.py
@@ -8,10 +8,11 @@ def waitForKeyPress():
         pass
 
 class OBJECT():
-    def __init__(self, hasHP = False):
+    def __init__(self, name, description, destroyedDescription = "", hasHP = False):
         assert isinstance(hasHP, bool), "hasHP needs to be True or False"
         self.hasHP = hasHP
-        self.description = ''
+        self.name   = name
+        self.setDescription(description)
         self.destroyedDescription = ''
 
     def setDescription(self, text):
@@ -54,8 +55,6 @@ class ROOM():
     def setDescription(self, text):
         temp = text.split()
         text = " ".join(temp)
-        print text
-
         for x in range(0, len(text)):
             if x % 120 == 0:
                 text = text[:x] + '\n' + text[x:]
@@ -64,6 +63,10 @@ class ROOM():
     def addToExits(self, addExits):
         for key, value in addExits.iteritems():
             self.exits[key] = value
+
+    def addObject(self, objects):
+        for key, value in objects.iteritems():
+            self.objects[key] = value
 
 class ROOMSETUP():
     def __init__(self):
@@ -121,9 +124,9 @@ class ROOMSETUP():
         doorStep.setExits(rooms, doorStepExits)
         doorStep.setDescription("You are on the doorstep of a terribly dark and evil fortress.\n"
                                 "Inside is the princess. As a Knight of the Order - you are tasked\n"
-                                "with saving her. Unfortunately, your training was cut short, as\n"
+                                "with her safety. Unfortunately, your training was cut short, as\n"
                                 "the other Knights were busy at the time. Good Luck, and may the\n"
-                                "Force be with you.\n")
+                                "Force be with you or some shit.\n")
 
         entryExits = ['Door Step',
                       'First Bed',
@@ -140,41 +143,25 @@ class ROOMSETUP():
         firstBed.setDescription("This room is poopy.")
         return rooms
 
-
-
-
-class INTERFACE():
+class COMMANDS():
     def __init__(self):
-        self.roomSetup      = ROOMSETUP()
-        self.allRooms       = self.roomSetup.allRooms
-        self.currentRoom    = self.allRooms['Door Step']
-        self.commands       = {"go"   : self.go,
-                               "help" : self.help}
+        self.commands       = {"go"     : self.go,
+                               "attack" : self.attack,
+                               "help"   : self.help,
+                               }
 
-    def displayRoom(self):
-        assert isinstance(self.currentRoom, ROOM), "Shit ain't a room bitch"
-        print self.currentRoom.getDescription()
-        print "Available exits are:"
-        print self.currentRoom.getExits()
-
-    def waitForAction(self):
-        text = raw_input("What would you like to do?")
-        text = text.lower()
-        for key, value in self.commands.iteritems():
-            if text.startswith(key.lower()):
-                self.commands[key](text)
-
-    def go(self, text):
+    def go(self, text, state):
         """
-        Is used to enter a room
+        Is used to enter a room.
+        For instance "go Entry"
         """
         text = text.replace('go ', '', 1)
         print text
-        for key, value in self.currentRoom.exits.iteritems():
+        for key, value in state.currentRoom.exits.iteritems():
             if text.startswith(key.lower()):
-                self.currentRoom = value
+                state.changeCurrentRoom(value)
 
-    def help(self, text):
+    def help(self, text, state):
         """
         Displays the commands.
         """
@@ -183,11 +170,64 @@ class INTERFACE():
             print key
             print self.commands[key].__doc__
 
-    def attack(self, text):
+    def attack(self, text, state):
+        """
+        Allows you to attack something.
+        """
         text = text.replace('attack ', '', 1)
-        for key, value in self.currentRoom.objects.iteritems():
-            if text.startswith(key.lower()) and value.hasHP():
-                print "You attack the {0} with your {1}.".format(key, self.player.currentWeapon)
+        for key, value in state.currentRoom.objects.iteritems():
+            if text.startswith(key.lower()):
+                if value.hasHP():
+                    print "You attack the {0} with your {1}.".format(key, state.player.currentWeapon)
+                else:
+                    print "You cannot attack the {0}".format(key)
+
+class STATE():
+    def __init__(self, currentRoom, player):
+        self.currentRoom    =   currentRoom
+        self.player         =   player
+
+    def changeCurrentRoom(self, newRoom):
+        self.currentRoom = newRoom
+
+
+class PLAYER():
+    def __init__(self):
+        self.weapon     = OBJECT("Fists", "You ball your hands into fists. Not the greatest weapon, but it'll have to do")
+        self.inventory  =   []
+
+    def addInventory(self, item):
+        assert isinstance(item, OBJECT), "Can't put non-objects in your backpack"
+        self.inventory.append(item)
+
+    def setWeapon(self, item):
+        assert isinstance(item, OBJECT), "Must equip object as items."
+        self.inventory.append(self.weapon)
+        if item in self.inventory:
+            self.inventory.remove(item)
+        self.weapon = item
+
+class INTERFACE():
+    def __init__(self):
+        self.roomSetup      = ROOMSETUP()
+        self.allRooms       = self.roomSetup.allRooms
+        self.currentState   = STATE(self.allRooms["Door Step"], PLAYER())
+        self.commands       = COMMANDS().commands
+
+
+    def displayRoom(self):
+        assert isinstance(self.currentState.currentRoom, ROOM), "Shit ain't a room bitch"
+        print self.currentState.currentRoom.getDescription()
+        print "Available exits are:"
+        print self.currentState.currentRoom.getExits()
+
+    def waitForAction(self):
+        text = raw_input("What would you like to do?")
+        text = text.lower()
+        for key, value in self.commands.iteritems():
+            if text.startswith(key.lower()):
+                self.commands[key](text, self.currentState)
+
 
 
 interface = INTERFACE()
